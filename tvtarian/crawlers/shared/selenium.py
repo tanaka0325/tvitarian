49bfe204c8f9a67f89d27b42af070b5eebd9c158
田中将平 <syouhei@jobby-MBP.local>
2018-06-10 19:40:16 +0900

    fix. comment in.

--- tvtarian/crawlers/shared/selenium.py
+++ tvtarian/crawlers/shared/selenium.py
@@ -3,8 +3,8 @@ from selenium.webdriver import Chrome, ChromeOptions
 
 def create_chrome_driver():
     options = ChromeOptions()
-    # options.binary_location = '/app/.apt/usr/bin/google-chrome'
-    # options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--no-sandbox')
+    options.binary_location = '/app/.apt/usr/bin/google-chrome'
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
     return Chrome(options=options)
